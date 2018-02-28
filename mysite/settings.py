bc1a1c5893451e72102e491a8eaabcf184a6bf8c
Jeroen Wernekinck <jeroen@wernekinck.nl>
2018-02-28 11:18:36 +0100

    My Djangy Django App, second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ggkh-ox7#*=go2+zr89i%hjg-v+$oa2n#zc(!s04!($-(9j#t+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
